6eac6fea828a09649bdee754eb1378e3df1090fa
odik91 <ali.shoddiqien@gmail.com>
2024-09-12 16:53:27 +0700

    adding register and send jwt

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,12 @@
+const { BadRequestError, UnauthenticatedError } = require('../errors')
 const User = require('../models/User')
-const { StatusCodes } = require('http-status-codes')
-const { BadRequestError } = require('../errors')
-const bcrypt = require('bcryptjs')
+const { StatusCodes } = require("http-status-codes")
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body })
-  res.status(StatusCodes.CREATED).json({ user })
+
+  const token = user.createJWT()
+  res.status(StatusCodes.CREATED).json({ token, user: {name: user.name} })
 }
 
 const login = async (req, res) => {
