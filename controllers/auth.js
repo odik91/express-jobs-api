c3f6d1a435c218edbf6c66d258479b9213e7afaa
odik91 <ali.shoddiqien@gmail.com>
2024-09-12 17:11:34 +0700

    adding login and send jwt

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,11 +6,32 @@ const register = async (req, res) => {
   const user = await User.create({ ...req.body })
 
   const token = user.createJWT()
-  res.status(StatusCodes.CREATED).json({ token, user: {name: user.name} })
+  res.status(StatusCodes.CREATED).json({ token, user: { name: user.name } })
 }
 
 const login = async (req, res) => {
-  res.send('login user')
+  const { email, password } = req.body
+
+  if (!email || !password) {
+    throw new BadRequestError('Please provide email and password')
+  }
+
+  const user = await User.findOne({ email })
+
+  //  check user
+  if (!user) {
+    throw new UnauthenticatedError("Invalid credentials")
+  }
+
+  // check password
+  const isCorrectPassword = await user.comparePassword(password)
+  if (!isCorrectPassword) {
+    throw new UnauthenticatedError('Invalid credentials')
+  }
+
+  const token = user.createJWT()
+  
+  res.status(StatusCodes.CREATED).json({ token, user: { name: user.name } })
 }
 
 module.exports = { register, login }
